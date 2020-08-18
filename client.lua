2d70ba2cc700a5007cb17db020bd3e41fd372bb1
İsmail KOÇ <ismailpvo1535@gmail.com>
2020-08-19 00:35:37 +0300

    Added Config file

--- client.lua
+++ client.lua
@@ -19,27 +19,39 @@ end)
 Citizen.CreateThread(function()
 	while true do
 		player = GetPlayerPed(-1)
-		Citizen.Wait(20 * 60000) -- 20 DAKİKA BEKLEME SÜRESİ - MAAŞ ALMA SÜRESİYLE AYNI
+		Citizen.Wait(Config.Timer * 60000) -- 20 DAKİKA BEKLEME SÜRESİ - MAAŞ ALMA SÜRESİYLE AYNI
 		ESX.PlayerData = ESX.GetPlayerData()
 		local salary  = ESX.PlayerData.job.grade_salary
 		tax = tax + 1
 		if tax == 2 then
 			ESX.TriggerServerCallback('torchizm:getCars', function(amount)
 				if amount > 0 and amount ~= 0 then
-					if ESX.PlayerData.job.name == "police" or ESX.PlayerData.job.name == "ambulance" or ESX.PlayerData.job.name == "sheriff" then
-						TriggerServerEvent('torchizm:memurPayCheck', amount + 5)
-						exports['mythic_notify']:SendAlert("inform", "Maaşından " .. amount + 5 .. "$ vergi kesildi!")
+					if Config.MotelTax == true then
+						if ESX.PlayerData.job.name == "police" or ESX.PlayerData.job.name == "ambulance" or ESX.PlayerData.job.name == "sheriff" then
+							TriggerServerEvent('torchizm:memurPayCheck', amount + Config.MotelTaxPay)
+							exports['mythic_notify']:SendAlert("inform", "Maaşından " .. amount + Config.MotelTaxPay .. "$ vergi kesildi!")
+						else
+							TriggerServerEvent('esx_billing:sendBill', -1, 'society_devlet', 'Devlet Vergisi (Araç + Motel)', amount + Config.MotelTaxPay)
+							exports['mythic_notify']:SendAlert("inform", amount + Config.MotelTaxPay .. "$ vergi geldi!")
+						end
 					else
-						TriggerServerEvent('esx_billing:sendBill', -1, 'society_devlet', 'Devlet Vergisi (Araç + Motel)', amount + 5)
-						exports['mythic_notify']:SendAlert("inform", amount + 5 .. "$ vergi geldi!")
+						if ESX.PlayerData.job.name == "police" or ESX.PlayerData.job.name == "ambulance" or ESX.PlayerData.job.name == "sheriff" then
+							TriggerServerEvent('torchizm:memurPayCheck', amount)
+							exports['mythic_notify']:SendAlert("inform", "Maaşından " .. amount .. "$ vergi kesildi!")
+						else
+							TriggerServerEvent('esx_billing:sendBill', -1, 'society_devlet', 'Devlet Vergisi (Araç)', amount)
+							exports['mythic_notify']:SendAlert("inform", amount .. "$ vergi geldi!")
+						end
 					end
 				else
-					if ESX.PlayerData.job.name == "police" or ESX.PlayerData.job.name == "ambulance" or ESX.PlayerData.job.name == 'sheriff' then
-						TriggerServerEvent('torchizm:memurPayCheck', 5)
-						exports['mythic_notify']:SendAlert("inform", "Maaşından " .. 5 .. "$ vergi kesildi!")
-					else
-						TriggerServerEvent('esx_billing:sendBill', -1, 'society_devlet', 'Devlet Vergisi (Motel)', 5)
-						exports['mythic_notify']:SendAlert("inform", "5$ vergi geldi!")
+					if Config.MotelTax == true then
+						if ESX.PlayerData.job.name == "police" or ESX.PlayerData.job.name == "ambulance" or ESX.PlayerData.job.name == 'sheriff' then
+							TriggerServerEvent('torchizm:memurPayCheck', Config.MotelTaxPay)
+							exports['mythic_notify']:SendAlert("inform", "Maaşından " .. Config.MotelTaxPay .. "$ vergi kesildi!")
+						else
+							TriggerServerEvent('esx_billing:sendBill', -1, 'society_devlet', 'Devlet Vergisi (Motel)', Config.MotelTaxPay)
+							exports['mythic_notify']:SendAlert("inform", Config.MotelTaxPay .. "$ vergi geldi!")
+						end
 					end
 				end
 			end, GetPlayerServerId(player))
---
+++ config.lua
@@ -0,0 +1,12 @@
+Config = {}
+
+-- 20 Dakikada bir vergi keser
+Config.Timer = 20
+
+-- Her araç başına alınacak vergi
+Config.TaxByVehicle = 5
+
+-- Motel vergisi kesilsin mi
+Config.MotelTax = true
+-- Motel vergisi fiyatı
+Config.MotelTaxPay = 5
