5caf9b4ecd401861ac912d760663172b895adc96
İsmail KOÇ <ismailpvo1535@gmail.com>
2020-08-19 00:57:05 +0300

    Hotfix

--- __resource.lua
+++ __resource.lua
@@ -7,6 +7,6 @@ client_scripts {
 
 server_scripts {
     '@mysql-async/lib/MySQL.lua',
-    'config.lua'
-    'server.lua',
+    'config.lua',
+    'server.lua'
 }
--- config.lua
+++ config.lua
@@ -1,6 +1,7 @@
 Config = {}
 
 -- 20 Dakikada bir vergi keser
+-- 2 Kere maaş verdiğinde 1 kere vergi keser
 Config.Timer = 20
 
 -- Her araç başına alınacak vergi
